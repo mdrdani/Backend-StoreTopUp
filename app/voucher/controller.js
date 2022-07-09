710caf48e67a2f60d12951a89c05aa3c4ea28d5b
mdrdani <sudokill3004@example.com>
2022-07-09 13:56:53 +0700

    Update status voucher

--- app/voucher/controller.js
+++ app/voucher/controller.js
@@ -215,4 +215,28 @@ module.exports = {
       res.redirect('/voucher');
     }
   },
+
+  actionStatus: async (req, res) => {
+    try {
+      const { id } = req.params;
+      let voucher = await Voucher.findOne({ _id: id });
+      let status = voucher.status === 'Y' ? 'N' : 'Y';
+      voucher = await Voucher.findOneAndUpdate(
+        {
+          _id: id,
+        },
+        {
+          status,
+        }
+      );
+
+      req.flash('alertMessage', 'Berhasil update status');
+      req.flash('alertStatus', 'success');
+      res.redirect('/voucher');
+    } catch (error) {
+      req.flash('alertMessage', `${error.message}`);
+      req.flash('alertStatus', 'danger');
+      res.redirect('/voucher');
+    }
+  },
 };
